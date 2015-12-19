e350c558fe1090db33545b2f369b398d994e5bee
shivaniRe <shivani0283@gmail.com>
2015-12-20 05:25:04 +0530

    Update advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,14 @@
-PLACE YOUR CODE HERE
+import re
+import csv
+
+f = open('faculty.csv')
+csv_f = csv.reader(f)
+csv_f.next()
+
+# List of email addresses
+parsed_data = [re.findall('[a-z0-9]*@\S*', row[3]) for row in csv_f]
+
+with open("emails.csv", "wb") as file:
+    writer = csv.writer(file)
+    writer.writerows(parsed_data)
+
